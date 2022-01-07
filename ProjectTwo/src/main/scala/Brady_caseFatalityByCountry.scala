2fb1ddb6fae6fa41feadee822ba50a120334f1ff
bdyson556 <71849300+bdyson556@users.noreply.github.com>
2022-01-07 14:28:24 -0600

    updated frequent syntax (#3)
    * updated frequent syntax
    
    * searching ALL countries (not just Peru and Bulgaria lol)
    
    * uncommented main .scala file

---
+++ ProjectTwo/src/main/scala/Brady_caseFatalityByCountry.scala
@@ -0,0 +1,52 @@
+import io.netty.util.ResourceLeakDetector.Level
+import org.apache.spark.sql._
+import org.apache.spark.sql.hive.test.TestHive.sparkContext
+import org.apache.spark.sql.functions._
+
+import sys.process._
+
+object Brady_caseFatalityByCountry {
+
+  def main(args: Array[String]): Unit = {
+
+
+    System.setProperty("hadoop.home.dir", "C:\\Hadoop")
+    val spark = SparkSession
+      .builder
+      .appName("Hello Hive")
+      .config("spark.master", "local")
+      .enableHiveSupport()
+      .getOrCreate()
+    println("created spark session")
+
+    spark.sparkContext.setLogLevel("ERROR")
+
+    spark.sql("DROP TABLE IF EXISTS TotalConfirmed")
+    spark.sql("CREATE TABLE TotalConfirmed(" +
+      "SNo INT," +
+      "ObservationDate STRING," +
+      "ProvinceorState STRING," +
+      "CountryorRegion STRING," +
+      "LastUpdate STRING," +
+      "Confirmed DOUBLE," +
+      "Deaths DOUBLE," +
+      "Recovered DOUBLE)" +
+      "row format delimited fields terminated by ','" +
+      "stored as textfile"
+    )
+
+    spark.sql(
+      "LOAD DATA LOCAL INPATH 'covid_19_data_complete(Kaggle).csv'" +
+      "OVERWRITE INTO TABLE TotalConfirmed"
+    )
+
+    spark.sql("SELECT FLOOR(sum(Confirmed)) as TotalConfirmed FROM TotalConfirmed")
+      .show()
+
+    spark.sql("SELECT CountryorRegion, " +
+      "MAX(Deaths) / MAX(Confirmed) AS country_mortality_rate " +
+      "FROM TotalConfirmed GROUP BY CountryorRegion ORDER BY 2 DESC").show()
+
+    spark.close()
+  }
+}
