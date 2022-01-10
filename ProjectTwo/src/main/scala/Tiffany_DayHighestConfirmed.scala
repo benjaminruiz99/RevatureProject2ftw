0f2949b5684a0bdb432c60c586bd608f3af596ca
TiffanyGRev <Godsgirl378@yahoo.com>
2022-01-10 13:50:45 -0800

    Total Recovered

--- ProjectTwo/src/main/scala/Tiffany_DayHighestConfirmed.scala
+++ ProjectTwo/src/main/scala/Tiffany_DayHighestConfirmed.scala
@@ -3,7 +3,7 @@ import org.apache.spark.sql.functions._
 
 import scala.sys.process._
 
-object DataPractice {
+object Tiffany_DayHighestConfirmed {
 
   def main(args: Array[String]): Unit = {
 
@@ -21,7 +21,10 @@ object DataPractice {
 
     spark.sql("CREATE TABLE IF NOT EXISTS TotalConfirmed(SNo INT, ObservationDate DATE, ProvinceorState STRING, CountryorRegion STRING, LastUpdate STRING, Confirmed DOUBLE, Deaths DOUBLE, Recovered DOUBLE) row format delimited fields terminated by ',' stored as textfile")
     spark.sql("LOAD DATA LOCAL INPATH 'covid_data_fixed.csv' OVERWRITE INTO TABLE TotalConfirmed")
-    spark.sql("SELECT FLOOR(sum(Confirmed)) as TotalConfirmed FROM TotalConfirmed").show()
+        spark.sql("SELECT ObservationDate, SUM(Confirmed) as Total FROM TotalConfirmed GROUP BY ObservationDate").createOrReplaceTempView("ConfirmedTotal")
+        spark.sql("SELECT ObservationDate, MAX(Total) as Max FROM ConfirmedTotal GROUP BY ").show
+        spark.sql("SELECT * FROM ConfirmedTotal").show
+
     spark.close()
   }
 }
--- ProjectTwo/src/main/scala/Tiffany_TotalRecovered.scala
+++ ProjectTwo/src/main/scala/Tiffany_TotalRecovered.scala
@@ -3,10 +3,9 @@ import org.apache.spark.sql.functions._
 
 import scala.sys.process._
 
-object DataPractice {
+object Tiffany_TotalRecovered {
 
   def main(args: Array[String]): Unit = {
-
 
     System.setProperty("hadoop.home.dir", "C:\\Hadoop")
     val spark = SparkSession
@@ -21,7 +20,9 @@ object DataPractice {
 
     spark.sql("CREATE TABLE IF NOT EXISTS TotalConfirmed(SNo INT, ObservationDate DATE, ProvinceorState STRING, CountryorRegion STRING, LastUpdate STRING, Confirmed DOUBLE, Deaths DOUBLE, Recovered DOUBLE) row format delimited fields terminated by ',' stored as textfile")
     spark.sql("LOAD DATA LOCAL INPATH 'covid_data_fixed.csv' OVERWRITE INTO TABLE TotalConfirmed")
-    spark.sql("SELECT FLOOR(sum(Confirmed)) as TotalConfirmed FROM TotalConfirmed").show()
+    spark.sql("SELECT MAX(Recovered) as TotalRecovered FROM TotalConfirmed GROUP BY ProvinceorState, CountryorRegion").createOrReplaceTempView("Recovered")
+    spark.sql("SELECT FLOOR(SUM(TotalRecovered)) as TotalRecovered FROM Recovered").show
+
     spark.close()
   }
 }
