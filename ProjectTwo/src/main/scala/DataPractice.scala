dd4029a52522226db01e930297d0b4f06ce4744f
TiffanyGRev <Godsgirl378@yahoo.com>
2022-01-10 09:52:21 -0800

    Total Recovered Cases

--- ProjectTwo/src/main/scala/DataPractice.scala
+++ ProjectTwo/src/main/scala/DataPractice.scala
@@ -1,9 +1,7 @@
-import io.netty.util.ResourceLeakDetector.Level
 import org.apache.spark.sql._
-import org.apache.spark.sql.hive.test.TestHive.sparkContext
 import org.apache.spark.sql.functions._
 
-import sys.process._
+import scala.sys.process._
 
 object DataPractice {
 
@@ -19,16 +17,15 @@ object DataPractice {
       .getOrCreate()
     println("created spark session")
 
-    spark.sparkContext.setLogLevel("WARN")
+    spark.sparkContext.setLogLevel("ERROR")
 
-    //spark.sql("CREATE TABLE IF NOT EXISTS POTDPartyData(ID INT, Rank INT, Name STRING, Server STRING, Datacenter STRING, Job STRING, Score INT, Floor INT, Date STRING) row format delimited fields terminated by ','")
-    //spark.sql("LOAD DATA LOCAL INPATH 'POTDParty.txt' INTO TABLE POTDPartyData")
-
-    val ConfirmedWorldWideDF = spark.read.options(Map("header" -> "true"))
-      .csv("time_series_covid_19_confirmed.csv")
-    ConfirmedWorldWideDF.show()
-
-
+    spark.sql("CREATE TABLE IF NOT EXISTS TotalConfirmed(SNo INT, ObservationDate DATE, ProvinceorState STRING, CountryorRegion STRING, LastUpdate STRING, Confirmed DOUBLE, Deaths DOUBLE, Recovered DOUBLE) row format delimited fields terminated by ',' stored as textfile")
+    spark.sql("LOAD DATA LOCAL INPATH 'covid_19_data_complete(Kaggle).csv' OVERWRITE INTO TABLE TotalConfirmed")
+//    spark.sql("SELECT FLOOR(sum(Confirmed)) as TotalConfirmed FROM TotalConfirmed").show()
+//    val dfConfirmedUS = spark.read.option("header", true).csv("time_series_covid_19_confirmed_US_complete(Kaggle).csv")
+//    dfConfirmedUS.show
+    spark.sql("SELECT MAX(Recovered) as TotalRecovered FROM TotalConfirmed GROUP BY ProvinceorState, CountryorRegion").createOrReplaceTempView("Recovered")
+    spark.sql("SELECT FLOOR(SUM(TotalRecovered)) FROM Recovered").show
 
     spark.close()
   }
