a58acf078f0bfe5cff7700cf77d9b1bc0f86f6b0
TiffanyGRev <Godsgirl378@yahoo.com>
2022-01-13 13:43:38 -0800

    Update DayHighest

--- ProjectTwo/src/main/scala/Tiffany_DayHighestConfirmed.scala
+++ ProjectTwo/src/main/scala/Tiffany_DayHighestConfirmed.scala
@@ -19,16 +19,24 @@ object Tiffany_DayHighestConfirmed {
 
     spark.sparkContext.setLogLevel("ERROR")
 
-    spark.sql("CREATE TABLE IF NOT EXISTS TotalConfirmed(SNo INT, ObservationDate DATE, ProvinceorState STRING, CountryorRegion STRING, " +
-      "LastUpdate STRING, Confirmed DOUBLE, Deaths DOUBLE, Recovered DOUBLE) row format delimited fields terminated by ',' stored as textfile")
-    spark.sql("LOAD DATA LOCAL INPATH 'covid_data_fixed.csv' OVERWRITE INTO TABLE TotalConfirmed")
-    spark.sql("SELECT ObservationDate, MAX(Confirmed) AS MaxConfirmed FROM TotalConfirmed " +
-      "GROUP BY ObservationDate ORDER BY MaxConfirmed").createOrReplaceTempView("Confirmed")
+    spark.sql("CREATE TABLE IF NOT EXISTS TotalConfirmed(SNo INT, ObservationDate DATE, " +
+      "ProvinceorState STRING, CountryorRegion STRING, " +
+      "LastUpdate STRING, Confirmed DOUBLE, Deaths DOUBLE, Recovered DOUBLE) " +
+      "row format delimited fields terminated by ',' stored as textfile")
+    spark.sql("LOAD DATA LOCAL INPATH 'covid_data_fixed.csv' " +
+      "OVERWRITE INTO TABLE TotalConfirmed")
+    spark.sql("SELECT ObservationDate, MAX(Confirmed) AS MaxConfirmed, ProvinceOrState, " +
+      "CountryOrRegion FROM TotalConfirmed GROUP BY ObservationDate, ProvinceOrState, CountryOrRegion"
+    ).createOrReplaceTempView("Confirmed")
     spark.sql("SELECT ObservationDate as Date, FLOOR(SUM(MaxConfirmed)) AS MaxConfirmed " +
-      "FROM Confirmed GROUP BY Date ORDER BY MaxConfirmed").createOrReplaceTempView("DailyConfirmed")
-    spark.sql("SELECT Date, MaxConfirmed, LAG(MaxConfirmed,1) OVER (ORDER BY MaxConfirmed ASC) " +
-      "AS PreviousMaxConfirmed FROM DailyConfirmed ORDER BY MaxConfirmed ASC").createOrReplaceTempView("DailyMaxConfirmed")
-    spark.sql("SELECT DATE_FORMAT(Date,\"M-d-y\") AS Date, MaxConfirmed - IFNULL(PreviousMaxConfirmed,0) AS Total" +
+      "FROM Confirmed GROUP BY Date " +
+      "ORDER BY MaxConfirmed ASC").createOrReplaceTempView("DailyConfirmed")
+    spark.sql("SELECT Date, MaxConfirmed, LAG(MaxConfirmed,1) " +
+      "OVER (ORDER BY MaxConfirmed ASC) " +
+      "AS PreviousMaxConfirmed FROM DailyConfirmed " +
+      "ORDER BY MaxConfirmed ASC").createOrReplaceTempView("DailyMaxConfirmed")
+    spark.sql("SELECT DATE_FORMAT(Date,\"M-d-y\") AS Date, " +
+      "MaxConfirmed - IFNULL(PreviousMaxConfirmed,0) AS Total" +
       " FROM DailyMaxConfirmed ORDER BY Total DESC LIMIT 1").show
 
     spark.close()
