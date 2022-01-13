336e537c135a01b443f09343dbc81a6e1e971ad0
TiffanyGRev <Godsgirl378@yahoo.com>
2022-01-13 07:17:38 -0800

    Fixed DataPractice

--- ProjectTwo/src/main/scala/DataPractice.scala
+++ ProjectTwo/src/main/scala/DataPractice.scala
@@ -1,4 +1,6 @@
+import io.netty.util.ResourceLeakDetector.Level
 import org.apache.spark.sql._
+import org.apache.spark.sql.hive.test.TestHive.sparkContext
 import org.apache.spark.sql.functions._
 
 import scala.sys.process._
@@ -19,8 +21,10 @@ object DataPractice {
 
     spark.sparkContext.setLogLevel("ERROR")
 
+    //spark.sql("DROP TABLE IF EXISTS TotalConfirmed")
     spark.sql("CREATE TABLE IF NOT EXISTS TotalConfirmed(SNo INT, ObservationDate DATE, ProvinceorState STRING, CountryorRegion STRING, LastUpdate STRING, Confirmed DOUBLE, Deaths DOUBLE, Recovered DOUBLE) row format delimited fields terminated by ',' stored as textfile")
     spark.sql("LOAD DATA LOCAL INPATH 'covid_data_fixed.csv' OVERWRITE INTO TABLE TotalConfirmed")
+
     spark.sql("SELECT FLOOR(sum(Confirmed)) as TotalConfirmed FROM TotalConfirmed").show()
     spark.close()
   }
